071712774ef54249d3440a12cd956aa7bde43f79
Mallory Feuer <mallory.feuer@gmail.com>
2018-03-16 19:19:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,7 @@ class Student
 
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
-    DB[:conn].execute(sql, name).map do |row|
+    what = DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
     binding.pry
