950acf63a02b2ef29ee9939d73a933615c29e8ca
Mallory Feuer <mallory.feuer@gmail.com>
2018-03-16 19:34:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,4 +71,9 @@ class Student
       self.new_from_db(row)
     end.first
   end
+
+  def self.all_students_in_grade_X(x)
+    sql = "SELECT * FROM students WHERE grade = ?"
+    DB[:conn].execute(sql, x)
+  end
 end
