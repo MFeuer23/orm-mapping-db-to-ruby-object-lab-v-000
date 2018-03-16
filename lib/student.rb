d41eb8009830b2e878d3ad9b346390510c2ff3ab
Mallory Feuer <mallory.feuer@gmail.com>
2018-03-16 19:31:20 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,4 +64,9 @@ class Student
     sql = "SELECT * FROM students WHERE grade = '10' LIMIT ?"
     DB[:conn].execute(sql, x)
   end
+
+  def self.first_student_in_grade_10
+    sql = "SELECT * FROM students WHERE grade = '10' LIMIT 1"
+    DB[:conn].execute(sql)
+  end
 end
